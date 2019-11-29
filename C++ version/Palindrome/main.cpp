579a2624428e2b581eafb1a0bb7c5ffe08dcc675
Abid Hasan Muin <muin.739@gmail.com>
2019-11-29 18:22:24 +0600

    Add files via upload
    Project link - https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
    
    *Project Statement*
    Return true if the given string is a palindrome. Otherwise, return false.
    
    A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
    
    Note
    You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
    
    We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
    
    We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
    
    *Test cases*
    palindrome("eye") should return a boolean.
    palindrome("eye") should return true.
    palindrome("_eye") should return true.
    palindrome("race car") should return true.
    palindrome("not a palindrome") should return false.
    palindrome("A man, a plan, a canal. Panama") should return true.
    palindrome("never odd or even") should return true.
    palindrome("nope") should return false.
    palindrome("almostomla") should return false.
    palindrome("My age is 0, 0 si ega ym.") should return true.
    palindrome("1 eye for of 1 eye.") should return false.
    palindrome("0_0 (: /-\ :) 0-0") should return true.
    palindrome("five|\_/|four") should return false.

---
+++ C++ version/Palindrome/main.cpp
@@ -0,0 +1,59 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <ctype.h>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    //Printing original string
+    cout << endl;
+    cout << "Original= " << str << endl;
+    cout << "String length= " << str.length() << endl;
+    cout << endl;
+
+    //To lower case conversion
+    for(unsigned int i=0; i<str.length(); i++)
+        str[i]=tolower(str[i]);
+    cout << "Case conversion= " << str << endl;
+    cout << "String length= " << str.length() << endl;
+    cout << endl;
+
+    //Remove whitespace
+    for(int k=0; k<str.length(); k++){
+        if(str[k] == ' ')
+            str.erase(k,1);
+    }
+    cout << "Remove whitespace " << str << endl;
+    cout << "String length " << str.length() << endl;
+    cout << endl;
+
+    //Remove alphanumeric
+    for(int l=0; l<str.length(); l++){
+        if(!isalnum(str[l]))
+            str.erase(l,1);
+    }
+    cout << "Remove alphanumeric= " << str << endl;
+    cout << "String length= " << str.length() << endl;
+    cout << endl;
+
+    //Palindrome check
+    int y=str.length()-1;
+    for(int m=0; m<str.length(); m++){
+        if(str[m]!=str[y--]){
+            cout << "false" << endl;
+            return 0;
+        }
+    }
+    cout << "true" << endl;
+
+    return 0;
+}
